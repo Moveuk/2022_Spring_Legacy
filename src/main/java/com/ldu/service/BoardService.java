3d17d32e7177804284fef2ba52c0fc7d6b83ce69
Moveuk <v_donguk@naver.com>
2022-04-17 13:22:17 +0900

    BoardService 작성 및 test

--- src/main/java/com/ldu/service/BoardService.java
+++ src/main/java/com/ldu/service/BoardService.java
@@ -1,9 +1,14 @@
 package com.ldu.service;
 
+import java.util.List;
+
 import com.ldu.model.BoardVO;
 
 public interface BoardService {
 
     /* 게시판 등록 */
     public void enroll(BoardVO board);
+    
+    /* 게시판 목록 */
+    public List<BoardVO> getList();
 }
