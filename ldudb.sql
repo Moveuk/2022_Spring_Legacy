f1b93c5feec4a25456fd92da177f7686282d6c6a
Moveuk <v_donguk@naver.com>
2022-04-17 10:30:55 +0900

    board 테이블 작성 & BoardVO 생성

---
+++ ldudb.sql
@@ -0,0 +1,25 @@
+create table board(
+    bno number not null,
+    title varchar2(150) not null,
+    content varchar2(2000) not null,
+    writer varchar2(50) not null,
+    regdate date default sysdate,
+    updatedate date default sysdate,
+    constraint pk_board PRIMARY key(bno)
+);
+
+-- sysdate : db 시간 넣기
+
+-- board 시퀀스 생성
+create sequence
+    board_seq
+    increment by 1
+    start with 1;
+    
+-- 테스트 db 정보
+insert into board(bno, title, content, writer) values (board_seq.nextval,'테스트 제목', '테스트 내용', '작가');
+ 
+select * from board;
+commit;
+--rollback;
+
